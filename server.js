0216e525d1baa5d152f89637a4adab11d6bca02c
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2020-04-03 19:53:17 -0300

    js att

--- server.js
+++ server.js
@@ -2,6 +2,37 @@ const express = require("express");
 
 const server = express();
 
+const ideas = [
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        title: "Curso de Programação",
+        category: "Estudos",
+        descripton: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti cum quo natus provident itaque quibusdam",
+        url: "#"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+        title: "Exercícios",
+        category: "Saúde",
+        descripton: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti cum quo natus provident itaque quibusdam",
+        url: "#"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+        title: "Meditação",
+        category: "Mentalidade",
+        descripton: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti cum quo natus provident itaque quibusdam",
+        url: "#"
+    }, 
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+        title: "Karaokê",
+        category: "Diversão em família",
+        descripton: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti cum quo natus provident itaque quibusdam",
+        url: "#"
+    }, 
+]
+
 server.use(express.static("public"));
 
 const nunjucks = require("nunjucks");
@@ -10,8 +41,20 @@ nunjucks.configure("views", {
     noCache: true,
 })
 
+
+
 server.get("/", function(req, res){
-    return res.render("index.html");
+
+    let lastIdeas = [];
+    for (let idea of ideas) {
+        if(lastIdeas.length < 3){
+            lastIdeas.push(idea)
+        }
+    }
+
+    lastIdeas = lastIdeas.reverse();
+
+    return res.render("index.html", { ideas: lastIdeas});
 })
 
 server.get("/ideias", function(req, res){ 
